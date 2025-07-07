0ee292a98bd0f6b38f4647ba660b6aa462fa07d1
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-07 17:42:57 +0900

    chore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
