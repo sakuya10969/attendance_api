dc1adb4a284efb8514f2ef95869cef71a7c73a5e
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-07 17:35:10 +0900

    fix: auth dto

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class SignInDto {
+  @ApiProperty({
+    description: 'メールアドレス',
+    example: 'test@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @ApiProperty({
+    description: 'パスワード',
+    example: 'password',
+  })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
