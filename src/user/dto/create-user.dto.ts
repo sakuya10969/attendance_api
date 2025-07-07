1e2476f717d3032788b66d073300c45c603cc3c1
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-07 17:35:24 +0900

    fix: create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
-  name: string;
+  @ApiProperty({
+    description: '名前',
+    example: 'test',
+  })
+  @IsString()
+  @IsNotEmpty()
+    name: string;
+
+  @ApiProperty({
+    description: 'メールアドレス',
+    example: 'test@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @ApiProperty({
+    description: 'パスワード',
+    example: 'password',
+  })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
