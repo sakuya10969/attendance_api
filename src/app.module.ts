4c99466248905883f4152d81a676888ccc59424c
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-23 18:36:14 +0900

    feat: break module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AttendanceModule } from './attendance/attendance.module';
+import { BreakModule } from './break/break.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule, AttendanceModule],
+  imports: [AuthModule, UserModule, PrismaModule, AttendanceModule, BreakModule],
   controllers: [AppController],
   providers: [AppService],
 })
