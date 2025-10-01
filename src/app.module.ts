9846c48e461012be96d0146a2c1224b3df746be7
sakuya10969 <sakuya.f20010618@gmail.com>
2025-10-01 17:14:37 +0900

    feat: add logger middleware for app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 
@@ -11,10 +11,17 @@ import { AttendanceModule } from './attendance/attendance.module';
 import { BreakModule } from './break/break.module';
 import { AttendanceDataModule } from './attendance-data/attendance-data.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LoggerMiddleware } from './middlewares/logger-middleware';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule, AttendanceModule, BreakModule, AttendanceDataModule, ConfigModule.forRoot({ isGlobal: true }),],
   controllers: [AppController],
   providers: [AppService, Reflector, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
