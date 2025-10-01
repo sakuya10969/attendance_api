0b2672ba44ac32faf1650d85a4d7f5e5e31aaf8b
sakuya10969 <sakuya.f20010618@gmail.com>
2025-10-01 17:14:13 +0900

    feat: logger middleware

---
+++ src/middlewares/logger-middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl } = req;
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      const status = res.statusCode;
+      console.log(
+        `[${method}] ${originalUrl} -> ${status} (${duration}ms)`
+      );
+    });
+
+    next();
+  }
+}
